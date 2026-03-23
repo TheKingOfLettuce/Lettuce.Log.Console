f5a544e57cf9acef3a13f8acd2860964759b97be
TheKingOfLettuce <68159617+TheKingOfLettuce@users.noreply.github.com>
2026-03-23 12:54:00 -0400

    Async Console Writer (#1)

--- Lettuce.Log.Console/ConsoleDestination.cs
+++ Lettuce.Log.Console/ConsoleDestination.cs
@@ -1,48 +1,80 @@
 using Lettuce.Log.Core;
 using System;
+using System.Threading.Channels;
+using System.Threading.Tasks;
 
 namespace Lettuce.Log.Console {
+    internal struct ConsoleLogEntry {
+        public string Message {get; set;}
+        public LogEventLevel LogLevel {get; set;}
+    }
+
 
     /// <summary>
     /// Takes log messages and writes them to <see cref="System.Console"/>
     /// Colors the logs based on the <see cref="LogEventLevel"/>
     /// </summary>
-    public class ConsoleDestination : ILogDestination
-    {
+    public sealed class ConsoleDestination : ILogDestination, IDisposable {
+        private readonly Channel<ConsoleLogEntry> _logChannel;
+        private readonly Task _consumeLoop;
+        private bool _isDisposed;
+
+        /// <summary>
+        /// Constructs our console destination
+        /// </summary>
+        public ConsoleDestination() {
+            _logChannel = Channel.CreateUnbounded<ConsoleLogEntry>();
+            _consumeLoop = Task.Run(ConsumerLoop);
+        }
+
         /// <summary>
         /// Logs the message to <see cref="System.Console"/>
         /// </summary>
         /// <param name="message">the message to log</param>
         /// <param name="level">the logging level to color with</param>
-        public void LogMessage(string message, LogEventLevel level)
-        {
-            ConsoleColor oldColor = System.Console.ForegroundColor;
+        public void LogMessage(string message, LogEventLevel level) {
+            _ = _logChannel.Writer.TryWrite(new ConsoleLogEntry{LogLevel=level, Message=message});
+        }
 
-            ConsoleColor logColor = oldColor;
+        private async Task ConsumerLoop() {
+            await foreach(ConsoleLogEntry logEntry in _logChannel.Reader.ReadAllAsync()) {
+                ConsoleColor oldColor = System.Console.ForegroundColor;
+                System.Console.ForegroundColor = GetConsoleColor(logEntry.LogLevel);
+                System.Console.WriteLine(logEntry.Message);
+                System.Console.ForegroundColor = oldColor;
+            }
+        }
+
+        private static ConsoleColor GetConsoleColor(LogEventLevel level) {
             switch (level) {
                 case LogEventLevel.VERBOSE:
-                    logColor = ConsoleColor.DarkGray;
-                    break;
+                    return ConsoleColor.DarkGray;
                 case LogEventLevel.DEBUG:
-                    logColor = ConsoleColor.Gray;
-                    break;
+                    return ConsoleColor.Gray;
                 case LogEventLevel.INFORMATION:
-                    logColor = ConsoleColor.White;
-                    break;
+                    return ConsoleColor.White;
                 case LogEventLevel.WARNING:
-                    logColor = ConsoleColor.Yellow;
-                    break;
+                    return ConsoleColor.Yellow;
                 case LogEventLevel.ERROR:
-                    logColor = ConsoleColor.Red;
-                    break;
+                    return ConsoleColor.Red;
                 case LogEventLevel.FATAL:
-                    logColor = ConsoleColor.DarkRed;
-                    break;
+                    return ConsoleColor.DarkRed;
+                default:
+                    return ConsoleColor.White;
+            }
+        }
+
+        /// <summary>
+        /// Disposes of our console destination ensuring all logs are written to the console
+        /// </summary>
+        public void Dispose() {
+            if (_isDisposed) {
+                return;
             }
 
-            System.Console.ForegroundColor = logColor;
-            System.Console.WriteLine(message);
-            System.Console.ForegroundColor = oldColor;
+            _isDisposed = true;
+            _logChannel.Writer.Complete();
+            _consumeLoop.Wait();
         }
     }
 
